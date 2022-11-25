357b413dc709842c75c1efde76eeab363ead59bb
KIM GYEOUNG HO <67896210+komool@users.noreply.github.com>
2022-11-25 16:02:10 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+	sort(vec.begin(), vec.end());
+	for (int j = 0; j < n; j++)
+	{
+		cout << vec[j] << "\n";
+	}
+
+	return 0;
+}
